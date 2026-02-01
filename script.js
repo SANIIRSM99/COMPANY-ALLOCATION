04249c949de51f45adeedd5041bc5d0516579dce
SANIIRSM99 <sanisani5959@gmail.com>
2026-02-01 12:33:22 +0500

    Update summary row in invoice popup

--- script.js
+++ script.js
@@ -1470,16 +1470,19 @@ popupRows += `<tr class="${rowClass} hover:bg-indigo-100 transition text-xs sm:t
     if (!popupRows) return;
 
     // --- Summary Footer Row ---
-   const summaryRow = `
-    <tr class="bg-indigo-100 font-bold text-xs sm:text-sm">
-        <td colspan="2" class="border p-2 text-center">TOTAL (${totalCustomers} Customers)</td>
-        <td class="border p-2">${totalItems} Items</td>
-        <td class="border p-2">${totalTarget}</td>
-        <td class="border p-2">${totalAchieved}</td>
-        <td class="border p-2">${totalRemaining}</td>
-        <td class="border p-2">  ${calculateSmartPerformance()}% </td> 
-        <td class="border p-2">${totalValue.toLocaleString()}</td>
-    </tr>`;
+  const summaryRow = `
+<tr class="bg-indigo-100 font-bold text-xs sm:text-sm">
+    <td colspan="4" class="border p-2 text-center">
+        TOTAL (${totalCustomers} Customers / ${totalItems} Items)
+    </td>
+
+    <td class="border p-2">${totalTarget}</td>
+    <td class="border p-2">${totalAchieved}</td>
+    <td class="border p-2">${totalRemaining}</td>
+    <td class="border p-2">${calculateSmartPerformance()}%</td>
+    <td class="border p-2">${totalValue.toLocaleString()}</td>
+</tr>`;
+
 
     let popup = document.getElementById("invoicePopup");
 
